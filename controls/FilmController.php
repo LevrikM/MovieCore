fcd2a5ddea99881b4773ecd5f103133bf2cdf204
barab <ipz213_bvv@student.ztu>
2023-04-22 00:23:18 +0300

    Vlad

--- controls/FilmController.php
+++ controls/FilmController.php
@@ -2,6 +2,14 @@
 include_once "../models/FilmModel.php";
 include_once "../library/mainFunctions.php";
 
+//Цей PHP-код виконує декілька дій, пов'язаних з відображенням та додаванням фільмів.
+// Він включає дві функції - showAction і addAction - та ще одну функцію filmAddAction,
+// яка просто відображає сторінку додавання фільму.
+
+
+//Функція showAction викликає функцію getFilmInfo, щоб отримати інформацію про фільм,
+// передану в параметрах запиту. Ця інформація потім передається в шаблон Smarty для відображення на сторінці фільму.
+// Крім того, функція встановлює форматування дати та часу для шаблону та відображає заголовок сторінки.
 function showAction($smarty){
     $id = isset($_GET["film"]) ? $_GET["film"] : null;
 
@@ -19,6 +27,9 @@ function showAction($smarty){
 }
 
 
+//Функція addAction перевіряє, чи були надіслані дані форми додавання фільму, та якщо так,
+// то додає новий фільм за допомогою функції addFilm.
+// Крім того, вона відображає сторінку додавання фільму з встановленим заголовком сторінки.
 function addAction($smarty){
     $name = isset($_POST["name"]) ? $_POST["name"] : null;
     $year = isset($_POST["year"]) ? $_POST["year"] : null;
@@ -48,8 +59,12 @@ function addAction($smarty){
 }
 
 
+//Функція filmAddAction просто відображає сторінку додавання фільму з встановленим заголовком сторінки.
 function filmAddAction($smarty){
     $smarty -> assign("pageTitle", "Додавання фільму");
     loadTemplate($smarty, "header");
     loadTemplate($smarty, "filmAdd");
 }
+//У цьому коді використовується шаблонізатор Smarty для відображення сторінок,
+// тому перед викликом функцій showAction та addAction, виконується завантаження шаблонів header,
+// filmShow та footer для сторінки фільму, а header, filmAdd та footer для сторінки додавання фільму.
